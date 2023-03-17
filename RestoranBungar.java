3889ccae99f914178ca5cd5113cd66de0f4ba82a
PapoyPopay <128023142+PapoyPopay@users.noreply.github.com>
2023-03-17 13:41:44 +0700

    Update RestoranBungar.java

--- RestoranBungar.java
+++ RestoranBungar.java
@@ -5,17 +5,20 @@ public class RestoranBungar {
     private static final DecimalFormat df = new DecimalFormat("0.00");
     private static final DecimalFormat df2 = new DecimalFormat("0");
     public static void main(String[] args){
+        
+        // Minta User Untuk Menginput Jumlah Orang dan Nama Pesanan
         Scanner input = new Scanner(System.in);
         System.out.println("Selamat siang..." + "\n\r" );
         System.out.print("Pesan untuk berapa orang : ");
         double jumlahOrang = input.nextDouble();
 
         System.out.print("Pesanan atas nama        : ");
-
         input.nextLine();
 
         String namaOrang = input.nextLine();
         System.out.println("\n\r");
+        
+        // Program Menampilkan Menu Makanan
         System.out.println("Menu Spesial hari ini");
         System.out.println("=====================" + "\n\r");
 
@@ -29,7 +32,8 @@ public class RestoranBungar {
         System.out.println("   3. Steak Sirloin Spesial            @ Rp. " + df.format(menu3));
         System.out.println("   4. Kwetiaw Siram Spesial            @ Rp. " + df.format(menu4));
         System.out.println("   5. Kambing Guling Spesial           @ Rp. " + df.format(menu5));
-        // input jumlah pesanan
+        
+        // Minta User Untuk Menginput Jumlah Pesanan
         System.out.println("\n\r" +"Pesanan Anda [batas pesanan 0-10 porsi]" + "\n\r");
         System.out.print("1. Nasi Goreng Spesial      = ");
         double jNG = input.nextDouble();
@@ -41,13 +45,15 @@ public class RestoranBungar {
         double jKS = input.nextDouble();
         System.out.print("5. Kambing Guling Spesial   = ");
         double jKG = input.nextDouble();
-
+        
+        //Hitung Pembelian Yang Dilakukan Oleh User
         double totNG = jNG * menu1;
         double totAB = jAB * menu2;
         double totSS = jSS * menu3;
         double totKS = jKS * menu4;
         double totKG = jKG * menu5;
-        // tampilan total pembelian
+        
+        //Tampilkan Harga Pembelian Dari Pesanan User
         System.out.println("\n\r" + "\n\r" + "Selamat menikmati makanan anda..." + "\n\r" + "\n\r" + "Pembelian :");
         System.out.println("\n\r" + "1. Nasi Goreng Spesial      = "+ df2.format(jNG) + " porsi * Rp. " + df.format(menu1) + "  = Rp.      " + df.format(totNG));
         System.out.println("2. Ayam Bakar Spesial       = "+ df2.format(jAB) + " porsi * Rp. " + df.format(menu2) + " = Rp.     " + df.format(totAB));
